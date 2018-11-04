209ff309b1e21896e1573949c23ae01cff9062a4
deXta <ddcruz@email.com>
2018-11-03 20:29:09 -0700

    Refactored using csv.reader, and removed the arg params

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,59 +1,51 @@
-import sys
+import os
+import csv
 
-def read_file(filename):
-    with open(filename, mode='rt', encoding='utf-8') as f:
-        #read the header, and convert to a list of columns
-        header = f.readline()[:-1] #ignore the \n at the end of the line
-        columns = header.split(',')
-        #adding a new column for the change
-        columns.append('change')
+csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
 
-        #converting each line as a row list and adding the new column for change
-        lines = [line[:-1] for line in f.readlines()]
-        rows = []
-        for x in range(len(lines)):
-            row = lines[x].split(',')
-            row.append('0')
-            rows.append(row)
-            if x > 0:
-                rows[x][2] = str(int(rows[x][1]) - int(rows[x-1][1]))
-        #return the lists of columns and rows
-        return columns, rows
+with open(csvpath, mode='r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #get the header
+    header = next(csvreader)
+    #add a new column called change
+    header.append('change')
 
-def output_results(filename, total_months, total_net_amount, total_change_in_proft_loss, max_increase_in_profit_month, max_increase_in_profit, max_decrease_in_profit_month, max_decrease_in_profit):
-    output = []
-    output.append("Financial Analysis")
-    output.append("-" * 50)
-    output.append(f"Total Months: {total_months}")
-    output.append(f"Total: ${total_net_amount}")
-    output.append(f"Average Change: ${round(total_change_in_proft_loss/(total_months - 1), 2)}")
-    output.append(f"Greatest Increase in Profits: {max_increase_in_profit_month} (${max_increase_in_profit})")
-    output.append(f"Greatest Decrease in Profits: {max_decrease_in_profit_month} (${max_decrease_in_profit})")
-
-    with open(filename, mode='wt', encoding='utf-8') as f:
-        for line in output:
-            f.write(line + '\n')
-            print(line)
-    
-def main(input_filename, output_filename):
-    columns, rows = read_file(input_filename)
+    totalMonths = 0
     total_net_amount = 0
-    total_change_in_proft_loss = 0
-    max_increase_in_profit = 0
-    max_increase_in_profit_month = ''
-    max_decrease_in_profit = 0
-    max_decrease_in_profit_month = ''
+
+    rows = []
+    for i, row in enumerate(csvreader):
+        totalMonths += 1
+        total_net_amount += int(row[header.index('Profit/Losses')])
+        #adding a default 0 value for the column change
+        row.append(0)
+        rows.append(row)
 
     for x in range(len(rows)):
-        total_net_amount += int(rows[x][columns.index('Profit/Losses')])
-        total_change_in_proft_loss += int(rows[x][columns.index('change')])
-        if max_increase_in_profit < int(rows[x][columns.index('change')]):
-            max_increase_in_profit_month = rows[x][columns.index('Date')]
-            max_increase_in_profit = int(rows[x][columns.index('change')])
-        if max_decrease_in_profit > int(rows[x][columns.index('change')]):
-            max_decrease_in_profit_month = rows[x][columns.index('Date')]
-            max_decrease_in_profit = int(rows[x][columns.index('change')])
-    output_results(output_filename, x + 1, total_net_amount, total_change_in_proft_loss, max_increase_in_profit_month, max_increase_in_profit, max_decrease_in_profit_month, max_decrease_in_profit)
+        if x > 0:
+            #calculating the value for change in each row, except the first row
+            rows[x][header.index('change')] = int(rows[x][header.index('Profit/Losses')]) - int(rows[x-1][header.index('Profit/Losses')])
 
-if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+#use comprehension to get the sum of change, and the divisor being the total row count of rows other than the first row
+sum_changes, total_changes = sum([row[header.index('change')] for i, row in enumerate(rows) if i > 0]), i
+#get the row with the greatest increase in profits
+most_profitable = max(rows, key=lambda item: item[2])
+#get the row with greatest decrease in losses
+worst_loss = min(rows,key=lambda item: item[2])
+
+#prep output
+output = []
+output.append('Financial Analysis')
+output.append('-' * 50)
+output.append(f'Total Months: {totalMonths}')
+output.append(f'Total: ${total_net_amount}')
+output.append(f"Average  Change: ${round(sum_changes/total_changes, 2)}")
+output.append(f"Greatest Increase in Profits: {most_profitable[header.index('Date')]} (${most_profitable[header.index('change')]})")
+output.append(f"Greatest Decrease in Profits: {worst_loss[header.index('Date')]} (${worst_loss[header.index('change')]})")
+
+outputfile = os.path.join('.', 'Resources', 'output.dat')
+
+with open(outputfile, mode='wt', encoding='utf-8') as f:
+    for line in output:
+        f.write(line + '\n')
+        print(line)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,49 +1,49 @@
-import sys
-from pprint import pprint as pp
-def read_file(filename):
-    with open(filename, mode='rt', encoding='utf-8') as f:
-       #read the header, and convert to a list of columns
-        header = f.readline()[:-1] #ignore the \n at the end of the line
-        columns = header.split(',')
+import os
+import csv
 
-        #converting each line into a row list
-        rows = [line[:-1].split(',') for line in f.readlines()]
-        
-    return columns, rows
+banner_length = 25
 
-def output_results(filename, total_votes, results):
-    banner_length = 25
-    output = []
-    output.append("Election Results")
-    output.append('-' * banner_length)
-    output.append(f"Total Votes: {total_votes}")
-    output.append('-' * banner_length)
-    for result in results:
-        output.append(f"{result[0]}: {result[1]:.3%} ({result[2]})")
-    output.append('-' * banner_length)
-    output.append(f"Winner: {results[0][0]}")
-    output.append('-' * banner_length)
-    with open(filename, mode='wt', encoding='utf-8') as f:
-        for line in output:
-            f.write(line + '\n')
-            print(line)
+csvpath = os.path.join('.', 'Resources', 'election_data.csv')
 
+with open(csvpath, mode='r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
 
-def main(input_filename, output_filename):
-    columns, rows = read_file(input_filename)
-    total_votes = len(rows)
-    #use set to get distinct candidates
-    candidates = (set(row[columns.index('Candidate')] for row in rows))
-    results = []
-    for candidate in candidates:
-        result = []
-        result.append(candidate)
-        total_votes_per_candidate = len([row for row in rows if row[columns.index('Candidate')] == candidate])
-        result.append(round(total_votes_per_candidate/total_votes, 5))
-        result.append(total_votes_per_candidate)
-        results.append(result)
-    sorted_results = sorted(results, key=lambda x:x[2], reverse=True)
-    output_results(output_filename, total_votes, sorted_results)
+    #get the header
+    header = next(csvreader)
+    #read the rest of the file
+    rows = [line for line in csvreader]
 
-if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+#A complete list of candidates who received votes
+candidates = set([row[header.index('Candidate')] for row in rows])
+#The total number of votes cast
+total_votes = len(rows)
+results = []
+for candidate in candidates:
+    result = []
+    result.append(candidate)
+    #The total number of votes each candidate won
+    total_votes_per_candidate = len([row for row in rows if row[header.index('Candidate')] == candidate])
+    #The percentage of votes each candidate won
+    result.append(total_votes_per_candidate/total_votes)
+    result.append(total_votes_per_candidate)
+    results.append(result)
+#sort the results by popular vote
+sorted_results = sorted(results, key=lambda x:x[2], reverse=True)
+
+banner_length = 25
+output = []
+output.append("Election Results")
+output.append('-' * banner_length)
+output.append(f"Total Votes: {total_votes}")
+output.append('-' * banner_length)
+for result in sorted_results:
+    output.append(f"{result[0]}: {result[1]:.3%} ({result[2]})")
+output.append('-' * banner_length)
+output.append(f"Winner: {sorted_results[0][0]}")
+output.append('-' * banner_length)
+
+output_filename = os.path.join('.', 'Resources', 'output.dat')
+with open(output_filename, mode='wt', encoding='utf-8') as f:
+    for line in output:
+        f.write(line + '\n')
+        print(line)
